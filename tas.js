dffdb75afb5fa458ef3e161c504de8a2f3ce5fd6
DT <66632141+DT-is-not-available@users.noreply.github.com>
2024-04-14 13:06:24 -0400

    Update tas.js

--- tas.js
+++ tas.js
@@ -1,11 +1,11 @@
-function tasSetup(c2) {
-	const notifications = document.createElement("div")
-	notifications.style = "pointer-events: none; position: fixed; left: 0px; right: 0px; top: 0px; bottom: 0px; font-family: monospace; white-space: pre;"
-	function notify(text, time) {
-		const notification = document.createElement("p")
-		notification.innerHTML = text
-		notifications.append(notification)
-		setTimeout(()=>notification.remove(), time*1000)
-	}
-	notify("TAS loaded", 3")
+const notifications = document.createElement("div")
+notifications.style = "pointer-events: none; position: fixed; left: 0px; right: 0px; top: 0px; bottom: 0px; font-family: monospace; white-space: pre;"
+
+function notify(text, time) {
+	const notification = document.createElement("p")
+	notification.innerHTML = text
+	notifications.append(notification)
+	setTimeout(()=>notification.remove(), time*1000)
 }
+
+notify("TAS loaded", 3")
